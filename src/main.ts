71a518960a79d13d25a3f9ee02b2095825717d63
Angel Romero <angelarvm@gmail.com>
2025-11-15 01:21:45 -0300

    chore(app): add swagger, implements OPEN API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port: number = Number(process.env.PORT ?? 3000);
+
+  // Configuring OPEN API
+  const config = new DocumentBuilder()
+    .setTitle('Auth - ABAC API')
+    .setDescription(
+      'This API that serves as an example of ABAC authz implementation',
+    )
+    .setVersion('1.0')
+    .addTag('ABAC')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
